fd7a0f6cbdcff94f24d3bc08bfeabc1782cd38e6
Kevin Castillo <kavier.jarvis@gmail.com>
2025-04-01 20:44:34 -0300

    feat: agrega lógica de compra de entradas

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
+    // Define las zonas disponibles
+    static int disponiblesPlateaAlta = 3;
+    static int disponiblesVip = 3;
+    static int disponiblesPlateaBaja = 3;
+    static int disponiblesPalcos = 3;
 
-    // Función para obtener el precio según tipo de entrada y tarifa
-    public static int obtenerPrecio(int tipoEntrada, int tipoTarifa) {
-        int[][] tablaPrecios = new int[4][2];
-
-        // VIP
-        tablaPrecios[0][0] = 20000; // Estudiante
-        tablaPrecios[0][1] = 30000; // Público general
-
-        // Platea baja
-        tablaPrecios[1][0] = 10000; // Estudiante
-        tablaPrecios[1][1] = 15000; // Público general
-
-        // Platea alta
-        tablaPrecios[2][0] = 9000;  // Estudiante
-        tablaPrecios[2][1] = 18000; // Público general
-
-        // Palcos
-        tablaPrecios[3][0] = 6500;  // Estudiante
-        tablaPrecios[3][1] = 13000; // Público general
-
-        return tablaPrecios[tipoEntrada][tipoTarifa];
-    }
-
-    // Función para validar que la entrada esté dentro de un rango
-    public static boolean esInputValido(int input, int minimo, int maximo) {
-        if (input >= minimo && input <= maximo) {
+    public static boolean isInputValid(int input, int min, int max) {
+        // TODO: agregar validación de tipo, para que maneje el error en caso de que ponga un string por ejemplo
+        if (input >= min && input <= max) {
             return true;
         } else {
             System.out.println("[Error] Ingresa un input válido, intenta nuevamente");
@@ -35,71 +19,120 @@ public class Main {
         }
     }
 
+    // Función para obtener el precio de la entrada
+    public static int obtenerPrecioEntradaPorZona(int zona) {
+        int[] tablaPrecios = new int[4];
+        tablaPrecios[0] = 30000; // VIP
+        tablaPrecios[1] = 15000; // Platea baja
+        tablaPrecios[2] = 18000; // Platea alta
+        tablaPrecios[3] = 13000; // Palcos
+
+
+        // restamos 1 a la zona ingresada para que haga match
+        // con el índice de la lista
+        return tablaPrecios[zona - 1];
+    }
+
+    public static boolean isAvailable(int zone) {
+        switch (zone) {
+            case 1: // Platea alta
+                return disponiblesPlateaAlta >= 0;
+            case 2: // Platea Vip
+                return disponiblesVip >= 0;
+            case 3: // Platea baja
+                return disponiblesPlateaBaja >= 0;
+            case 4: // Palcos
+                return disponiblesPalcos >= 0;
+            default:
+                return false;
+        }
+    }
+
+    private static String obtenerZona(int zone) {
+        if (!isInputValid(zone, 1, 4)) return null;
+
+        return switch (zone) {
+            case 1 -> "Platea alta";
+            case 2 -> "Vip";
+            case 3 -> "Platea baja";
+            case 4 -> "Palcos";
+            default -> null;
+        };
+    }
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int tipoEntrada = 0, tipoTarifa = 0;
-        int precioNormal, precioEstudiante;
-        int descuentoEstudiantil, tasaDescuento;
+        Scanner sc = new Scanner(System.in);
 
-        // Solicitar tipo de entrada
+        double STUDENT_DISCOUNT = 0.15; // descuento de estudiante
+        double SENIOR_DISCOUNT = 0.10; // descuento de adulto mayor
+
+        double precioFinalEntrada = 0;
+
+        System.out.println("MENU");
+        int option;
+
         do {
-            System.out.println("=== Tipos de entrada ===");
-            System.out.println("0. Vip");
-            System.out.println("1. Platea baja");
-            System.out.println("2. Platea alta");
-            System.out.println("3. Palcos");
+            System.out.println("1. Comprar entrada");
+            System.out.println("2. Salir");
 
-            System.out.print("Ingresa el tipo de entrada: ");
-            try {
-                tipoEntrada = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("[Error] Debes ingresar un número");
-                scanner.nextLine(); // Limpiar buffer
-                tipoEntrada = -1; // Valor que no pasará la validación
-            }
-        } while (!esInputValido(tipoEntrada, 0, 3));
+            System.out.print("Ingresa la opción que deseas: ");
+            option = sc.nextInt();
+        } while (!isInputValid(option, 1, 2));
 
-        // Solicitar tipo de tarifa
-        do {
-            System.out.println("=== Tipos de tarifas ===");
-            System.out.println("0. Estudiante");
-            System.out.println("1. Público general");
-
-            System.out.print("Ingresa el tipo de tarifa: ");
-            try {
-                tipoTarifa = scanner.nextInt();
-            } catch (Exception e) {
-                System.out.println("[Error] Debes ingresar un número");
-                scanner.nextLine(); // Limpiar buffer
-                tipoTarifa = -1; // Valor que no pasará la validación
-            }
-        } while (!esInputValido(tipoTarifa, 0, 1));
-
-        // Obtener precios
-        precioNormal = obtenerPrecio(tipoEntrada, 1);
-        precioEstudiante = obtenerPrecio(tipoEntrada, 0);
-
-        // Calcular el descuento por estudiante
-        if (tipoTarifa == 0) {
-            descuentoEstudiantil = precioNormal - precioEstudiante;
-        } else {
-            descuentoEstudiantil = 0;
+        // Verifica que el usuario uso la opción de salir y retorna o sale
+        // del programa
+        if (option == 2) {
+            System.out.println("Gracias por usar el programa :) Hasta luego!");
+            return;
         }
 
-        // Calcular tasa de descuento
-        tasaDescuento = (int)((descuentoEstudiantil * 100) / precioNormal);
+        // no agrego un else if para no anidar código dentro de otro scope
+        if (option == 1) {
+            System.out.println("=== ZONAS DISPONIBLES ===");
 
-        // Mostrar resultados
-        System.out.println("Precio normal: $" + precioNormal);
-        System.out.println("------------------------");
-        System.out.println("Descuento (%): " + tasaDescuento + "%");
-        System.out.println("Descuento a aplicar: $" + descuentoEstudiantil);
-        System.out.println("------------------------");
-        System.out.println("Total a pagar: $" + (precioNormal - descuentoEstudiantil));
-        System.out.println("==========================");
+            System.out.println("1. Platea alta: " + disponiblesPlateaAlta);
+            System.out.println("2. Vip: " + disponiblesVip);
+            System.out.println("3. Platea baja: " + disponiblesPlateaBaja);
+            System.out.println("4. Palcos: " + disponiblesPalcos);
 
-        System.out.println("Gracias por su compra, disfrute la función");
+            int inputZone;
 
-        scanner.close();
+            do {
+                System.out.print("Ingresa la zona que desea comprar: ");
+                inputZone = sc.nextInt();
+            } while (!isInputValid(inputZone, 1, 4) && !isAvailable(inputZone));
+
+            // Asigna el precio general de la entrada sin descuento según
+            // la zona escogida
+            double precioGeneralEntrada = obtenerPrecioEntradaPorZona(inputZone);
+
+            int edad;
+
+            // Validamos que la edad sea válida
+            do {
+                System.out.print("Ingresa tu edad: ");
+                edad = sc.nextInt();
+            } while (!isInputValid(edad, 1, 100));
+
+            double descuentoAplicado = 0;
+
+            if (edad <= 18) {
+                descuentoAplicado = precioGeneralEntrada * STUDENT_DISCOUNT;
+                precioFinalEntrada = precioGeneralEntrada - descuentoAplicado;
+            } else if (edad >= 60) {
+                descuentoAplicado = precioGeneralEntrada * SENIOR_DISCOUNT;
+                precioFinalEntrada = precioGeneralEntrada - descuentoAplicado;
+            } else {
+                precioFinalEntrada = precioGeneralEntrada;
+            }
+
+            System.out.println("=== RESUMEN DE TU COMPRA ===");
+            System.out.println("Ubicación (Zona): " + obtenerZona(inputZone));
+            System.out.println("Precio base: " + obtenerPrecioEntradaPorZona(inputZone));
+            System.out.println("Descuento aplicado: " + descuentoAplicado);
+            System.out.println("Precio final a pagar: " + precioFinalEntrada);
+        }
+
+        sc.close();
     }
 }
